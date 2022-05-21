4141b61ccb2a519e1031c0af8d17ce0ecb154314
Aigbe <aigbedestinyic@gmail.com>
2022-05-21 11:36:04 +0100

    Finished first stag

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "https://thawing-castle-70547.herokuapp.com",
+  baseURL: "https://thawing-castle-70547.herokuapp.com/api/v1",
 });
 export default instance;
